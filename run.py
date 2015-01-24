3d03dd4c286f1dd82f3dcfbc9ce8a2f79545f65d
laike9m <laike9m@gmail.com>
2015-01-24 11:35:25 +0800

    whatever

--- run.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-import upyun_api
-from os.path import expanduser, join
-
-home = expanduser("~")
-upyun_api.sync_folder(join(home, 'fbt_server_py', 'static', 'images'),
-                      '/static/images')
